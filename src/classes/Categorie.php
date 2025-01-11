999a1a64d6644de2853f33dc4bf7ed5d88c0937d
ouahmane youness <youness.ouahmane4@gmail.com>
2025-01-11 16:44:24 +0100

    edit delte categorie

---
+++ src/classes/Categorie.php
@@ -0,0 +1,50 @@
+<?php
+
+
+
+class Categorie
+{
+
+    private $id;
+    private $name;
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;       
+    }
+
+    public function setattributes($name)
+    {
+        $this->name= $name;
+    }
+
+    public function createCategory() {
+        $query = "INSERT INTO categories (name) VALUES (:name)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':name', $this->name);
+        return $stmt->execute();
+    }
+
+    public function getAllCategories() {
+        $query = "SELECT * FROM categories";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function editCategory($id, $newname) {
+            $query = "UPDATE categories SET name = :newname WHERE id = :id";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(':name', $newname);
+            $stmt->bindParam(':id', $id);
+            return $stmt->execute();
+    }
+
+    public function deleteCategory($id) {
+            $query = "DELETE FROM categories WHERE id = :id";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(':id', $id);
+            return $stmt->execute();
+    }
+}
