aec8faabba3e130a94fed61f267ced4323347a77
ouahmane youness <youness.ouahmane4@gmail.com>
2025-01-11 14:31:40 +0100

    setattributes users

--- src/classes/User.php
+++ src/classes/User.php
@@ -4,13 +4,30 @@
 
 class Users
 {
-    protected $db;
-    protected $id;
-    protected $name;
-    protected $email;
-    protected $password;
-    protected $role;
-    protected $isaproved;
-    protected $profil;
+    private $db;
+    private $id;
+    private $name;
+    private $email;
+    private $password;
+    private $role;
+    private $isApproved;
+    private $createdAt;
+
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function setAttributes($name, $email, $password, $role ='student')
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+        $this->isApproved = ($role === 'teacher') ? false : true;
+        $this->createdAt = date('Y-m-d H:i:s');
+    }
+
 
 }
