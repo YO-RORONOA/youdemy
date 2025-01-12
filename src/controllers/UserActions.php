77040449e8fe8de05bb4b247db3cf95c8c2add5e
ouahmane youness <youness.ouahmane4@gmail.com>
2025-01-12 13:20:49 +0100

    add useraction controller

--- src/controllers/UserActions.php
+++ src/controllers/UserActions.php
@@ -10,10 +10,45 @@ class UserActions
     {
         $database = new Database;
         $this->db = $database->connect();
+        $this->user = new Users($this->db);
+    }
+
+    public function activateUser($id)
+    {
+        return $this->user->updateUserstatus($id, 'active');
+    }
+
+    public function suspendUser($id)
+    {
+        return $this->user->updateUserstatus($id, 'suspended');
+    }
+
+    public function deleteUser($id)
+    {
+        return $this->user->updateUserstatus($id, 'deleted');
     }
 
 
-    
 
+    public function handleRequest($action, $userId)
+    {
+        $result = false;
+        switch ($action) {
+            case 'activate':
+                $result = $this->activateUser($userId);
+                break;
+            case 'suspend':
+                $result = $this->suspendUser($userId);
+                break;
+            case 'delete':
+                $result = $this->deleteUser($userId);
+                break;
+        }
 
+        return $result;
+    }
 }
+
+
+
+
